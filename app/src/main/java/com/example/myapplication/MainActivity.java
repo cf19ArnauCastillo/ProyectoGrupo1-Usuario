37a792c7b652d61f7f4bd540256c2ec57bcae286
Laura <cf20laura.martin@iesjoandaustria.org>
2021-11-17 20:09:55 +0100

    first steps

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSigIn = findViewById(R.id.btnSingIn);
+        EditText textMail = findViewById(R.id.txtMail);
+        EditText txtPassword = findViewById(R.id.txtPassword);
+
+
+        // If the user already access into the app, goes directly to home screen
+        SharedPreferences prefs = getSharedPreferences("SharedP", Context.MODE_PRIVATE);
+
+        if (prefs.getBoolean("login", false)) startHome();
+
+
+        /**
+         * When there is a click on the "Login" button and the credentials are correct,
+         * home screen will take place.
+         */
+        btnSigIn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (textMail.getText().toString().equals("123") && txtPassword.getText().toString().equals("123")) {
+                    savePreferences(prefs, textMail.getText().toString());
+                    startHome();
+                } else {
+                    Toast.makeText(getApplicationContext(), "The password is incorrect", Toast.LENGTH_LONG).show();
+                    txtPassword.setText("");
+                }
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+        public void startHome () {
+            startActivity(new Intent(getApplicationContext(), MenuFragment.class));
+        }
+        public void startRegister () {
+            startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+        }
     }
 }

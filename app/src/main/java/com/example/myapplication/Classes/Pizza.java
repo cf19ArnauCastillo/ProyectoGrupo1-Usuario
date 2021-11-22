3306f261ae2259c8076b1704a4748133286a87fc
Adam <cf19adan.montesinos@iesjoandaustria.org>
2021-11-22 18:46:31 +0100

    tipo en clase

--- app/src/main/java/com/example/myapplication/Classes/Pizza.java
+++ app/src/main/java/com/example/myapplication/Classes/Pizza.java
@@ -5,15 +5,18 @@ import java.io.Serializable;
 public class Pizza implements Serializable {
     public String pizzaname;
     public String description;
+    public String tipo;
     public int price;
 
     public Pizza(){
     }
 
-    public Pizza(String playerName, int age, String position, int shirtNo, int goal) {
+    public Pizza(String pizzaname, String description, String tipo, int price) {
         this.pizzaname = pizzaname;
         this.description = description;
+        this.tipo = tipo;
         this.price = price;
+
 
     }
 
@@ -31,6 +34,14 @@ public class Pizza implements Serializable {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 
     public int getPrice() {

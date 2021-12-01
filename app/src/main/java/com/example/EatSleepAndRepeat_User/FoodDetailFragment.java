74147e6c79ac1ae398a6c4f54be6dc46e171394d
cf19ArnauCastillo <cf19arnau.castillo@iesjoandaustria.org>
2021-12-01 17:22:47 +0100

    WIP: Making food detail

---
+++ app/src/main/java/com/example/EatSleepAndRepeat_User/FoodDetailFragment.java
@@ -0,0 +1,26 @@
+package com.example.EatSleepAndRepeat_User;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Scroll view in Fragment
+ */
+public class FoodDetailFragment extends Fragment {
+
+    public FoodDetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_food_detail, container, false);
+    }
+}

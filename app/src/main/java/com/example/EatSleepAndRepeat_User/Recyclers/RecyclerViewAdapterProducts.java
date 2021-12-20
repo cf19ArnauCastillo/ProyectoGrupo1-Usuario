75d0078cb9d33d311a1b540c89b0a77a0c27ad6e
cf19ArnauCastillo <cf19arnau.castillo@iesjoandaustria.org>
2021-12-20 17:46:17 +0100

    wip food detail

--- app/src/main/java/com/example/EatSleepAndRepeat_User/Recyclers/RecyclerViewAdapterProducts.java
+++ app/src/main/java/com/example/EatSleepAndRepeat_User/Recyclers/RecyclerViewAdapterProducts.java
@@ -1,25 +1,30 @@
 package com.example.EatSleepAndRepeat_User.Recyclers;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
-
+import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.bumptech.glide.Glide;
 import com.example.EatSleepAndRepeat_User.Classes.Dish;
-
+import com.example.EatSleepAndRepeat_User.DB.DBHelper;
+import com.example.EatSleepAndRepeat_User.FragmentFoodDetail;
 import com.example.EatSleepAndRepeat_User.R;
 import com.example.EatSleepAndRepeat_User.SQLITE.CartList;
 import com.example.EatSleepAndRepeat_User.SQLITE.CartListDBHelper;
@@ -46,15 +51,16 @@ public class RecyclerViewAdapterProducts extends RecyclerView.Adapter<RecyclerVi
         this.context = context;
     }
 
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_products, parent, false);
         ViewHolder holder = new ViewHolder(view);
         return holder;
+
     }
 
-    //okay
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.name.setText(dishes.get(position).getName());
@@ -69,6 +75,21 @@ public class RecyclerViewAdapterProducts extends RecyclerView.Adapter<RecyclerVi
             @Override
             public void onSuccess(Uri downloadUrl){
                 Glide.with(context).load(downloadUrl).into(holder.image);
+            }
+        });
+        // Cuando le clickas a la imagen te lleva al food detail fragment
+        holder.image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Guardas la infomacion en una "caja"
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("dish", dishes.get(position));
+                //Mandas la informacion a la nueva pantalla
+                AppCompatActivity app = (AppCompatActivity) v.getContext();
+                FragmentFoodDetail fragmentFoodDetail = new FragmentFoodDetail();
+                fragmentFoodDetail.setArguments(bundle);
+
+                app.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragmentFoodDetail).commit();
             }
         });
 
@@ -140,6 +161,8 @@ public class RecyclerViewAdapterProducts extends RecyclerView.Adapter<RecyclerVi
         ImageView image;
         Button btnAddProduct;
         TextView txtNumber;
+        Button btnRemove;
+        Button btnAdd;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
